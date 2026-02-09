4b99eb51405f4463baa6fa54b9738aa1d03daaed
Shyam Jee <shyamjee969678@gmail.com>
2026-02-09 20:53:04 +0530

    Change API key in URL constant
    Updated the API key in the URL constant.

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export let URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
+export let URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCxYqjaeR8SuGCc85odZZDA0FJhasSxb_I";
